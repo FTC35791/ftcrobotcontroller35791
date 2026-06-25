d80011f0490a226113fa4496880ed50228335b69
Your Name <ftc35791@gmail.com>
2026-06-25 12:08:45 -0500

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GitTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GitTest.java
@@ -1,5 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
-public class GitTest {
-//test test test
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp()
+public class GitTest extends OpMode {
+
+    private DcMotor rightDrive;
+    private DcMotor leftDrive;
+
+    @Override
+    public void init() {
+        rightDrive = hardwareMap.get(DcMotor.class,"Maddie");
+        leftDrive = hardwareMap.get(DcMotor.class,"Niralya");
+    }
+
+    @Override
+    public void loop() {
+        int x = 5;
+
+        if (gamepad1.left_stick_y >= 0.5) {
+            setPowers(0.5,0.5); // sets wheel speed
+        }
+
+    }
+
+    private void setPowers(double rightPower, double leftPower) {
+        rightDrive.setPower(rightPower);
+        leftDrive.setPower(leftPower);
+    }
+
 }
