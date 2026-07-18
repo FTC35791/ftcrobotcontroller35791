728ea4aec6d87f996cf0aadc699288bd721ee6ef
Your Name <ftc35791@gmail.com>
2026-07-18 11:10:14 -0500

    .

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Drive extends OpMode {
+
+    //declaring motor variables
+    private DcMotor rightDrive;
+    private DcMotor leftDrive;
+
+    @Override
+    public void init() {
+        //calling hardware init (see bottom of code)
+        hardwareInit();
+    }
+
+    @Override
+    public void loop() {
+        double forward = gamepad1.left_stick_y;
+        double turn = gamepad1.left_stick_x;
+
+
+
+    }
+
+    public void hardwareInit() {
+        //setting motor variables to the actual motors in the robot
+        rightDrive = hardwareMap.get(DcMotor.class,"rightDrive");
+        leftDrive = hardwareMap.get(DcMotor.class,"leftDrive");
+
+        //setting motor zero power behavior to brake
+        rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+}
+
