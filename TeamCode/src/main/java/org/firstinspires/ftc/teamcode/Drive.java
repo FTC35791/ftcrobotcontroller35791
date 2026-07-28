e605cf28a48f1ac62a8f691cc37e4459dbd96ebb
Maddie <maddie32612@gmail.com>
2026-07-28 18:38:19 -0500

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -1,13 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+@TeleOp()
 public class Drive extends OpMode {
 
     //declaring motor variables
     private DcMotor rightDrive;
     private DcMotor leftDrive;
+    private DcMotor flyWheel;
 
     @Override
     public void init() {
@@ -19,9 +22,13 @@ public class Drive extends OpMode {
     public void loop() {
         double forward = gamepad1.left_stick_y;
         double turn = gamepad1.left_stick_x;
+        double launcherSpeed = gamepad1.right_stick_y;
 
-        leftDrive.setPower(-(forward));
-        rightDrive.setPower(forward);
+        leftDrive.setPower(-(forward+turn)/2);
+        rightDrive.setPower((forward-turn)/2);
+
+        flyWheel.setPower(launcherSpeed/2);
+
 
     }
 
@@ -29,7 +36,7 @@ public class Drive extends OpMode {
         //setting motor variables to the actual motors in the robot
         rightDrive = hardwareMap.get(DcMotor.class,"rightDrive");
         leftDrive = hardwareMap.get(DcMotor.class,"leftDrive");
-
+        flyWheel = hardwareMap.get(DcMotor.class,"flywheel");
         //setting motor zero power behavior to brake
         rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
