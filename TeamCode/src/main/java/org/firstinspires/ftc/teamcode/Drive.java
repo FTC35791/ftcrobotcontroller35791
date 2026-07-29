64bdd8d70a597e4b284a14cc2d46af706dd0cde9
Maddie <maddie32612@gmail.com>
2026-07-29 10:00:19 -0500

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.*;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp()
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hi.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hi.java
@@ -11,7 +11,7 @@ public class hi extends OpMode {
 
     @Override
     public void init() {
-    }
+
         hardwareInit();
     }
 
