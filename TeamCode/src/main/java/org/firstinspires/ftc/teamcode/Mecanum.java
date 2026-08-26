e19b83f90d46773c122ae12a97729013d7e5a81b
Maddie <maddie32612@gmail.com>
2026-08-26 18:15:01 -0500

    y do i have to write this

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+@TeleOp
 public class Mecanum extends OpMode {
     private DcMotor rightFront, leftFront, rightBack, leftBack;
     private double power;
@@ -24,13 +26,13 @@ public class Mecanum extends OpMode {
         power = (Math.sqrt(gamepad1.left_stick_x * gamepad1.left_stick_x) + (gamepad1.left_stick_y * gamepad1.left_stick_y));
         theta = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x);
         sin = Math.sin(theta - Math.PI / 4);
-        cos = Math.sin(theta - Math.PI / 4);
+        cos = Math.cos(theta - Math.PI / 4);
         max = Math.max(Math.abs(sin), Math.abs(cos));
 
-        leftFrontPower = (power * cos / max + turn);
-        rightFrontPower = (power * sin / max + turn);
-        leftBackPower = (power * sin / max + turn);
-        rightBackPower = (power * cos / max + turn);
+        leftFrontPower = (power * sin / max + turn);
+        rightFrontPower = (power * cos / max + turn);
+        leftBackPower = (power * cos / max + turn);
+        rightBackPower = (power * sin / max + turn);
 
         if((power + Math.abs(turn)) > 1) {
             leftFrontPower /= power + turn;
@@ -39,9 +41,10 @@ public class Mecanum extends OpMode {
             rightBackPower /= power + turn;
         }
 
+        //two motors need to be negative
         leftFront.setPower(leftFrontPower);
-        rightFront.setPower(rightFrontPower);
-        leftBack.setPower(leftBackPower);
+        rightFront.setPower(-rightFrontPower);
+        leftBack.setPower(-leftBackPower);
         rightBack.setPower(rightBackPower);
 
     }
