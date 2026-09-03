37ebd004230b868a0b023ca44b21913b5effe671
Maddie <maddie32612@gmail.com>
2026-09-02 19:10:59 -0500

    y do i have to write this

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intakeTest.java
@@ -1,4 +1,41 @@
 package org.firstinspires.ftc.teamcode;
 
-public class intakeTest {
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class intakeTest extends OpMode{
+
+
+    private DcMotor leftFront;
+    private GoBildaPinpointDriver pinpoint;
+    private double power;
+    private double theta;
+    private double sin;
+    private double cos;
+    private double max;
+    private double turn;
+    private double currentHeading;
+
+    @Override
+    public void init() {
+        hardwareInit();
+    }
+
+    @Override
+    public void loop() {
+
+       leftFront.setPower(gamepad1.left_stick_y/2);
+
+    }
+
+    public void hardwareInit() {
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
 }
+
+
